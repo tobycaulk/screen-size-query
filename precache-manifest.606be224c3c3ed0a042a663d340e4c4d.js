2a563d7de820d38b01687b2f77a30f25ebf16e61
Toby Caulk <tobycaulk@gmail.com>
2020-11-15 20:21:24 -0500

    Updates

---
+++ precache-manifest.606be224c3c3ed0a042a663d340e4c4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966d0e68fc0bc7bfc4a7e482d8906538",
+    "url": "./index.html"
+  },
+  {
+    "revision": "41dfae51eb72f9a7d82f",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "81bcf7d259638d5b7224",
+    "url": "./static/js/2.7baff1bd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.7baff1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41dfae51eb72f9a7d82f",
+    "url": "./static/js/main.e111de4f.chunk.js"
+  },
+  {
+    "revision": "8910b9edf7070335534b",
+    "url": "./static/js/runtime-main.7ced3d1d.js"
+  }
+]);
